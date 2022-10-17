dbd988767e94c27fe86ebff4ece508b36cc2aafb
Seongyeon Lee <winkite1226@gmail.com>
2022-10-17 16:28:31 +0900

    complete signup FE

--- post/views.py
+++ post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main(request):
+    if request.method == 'GET':
+        return render(request, 'post/main.html')
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+            return render(request, 'user/signup.html')
+
+def signin(request):
+    if request.method == 'GET':
+            return render(request, 'user/signin.html')
