9352739159f190e7b1888ac0677c4fe1c49fdd36
Seongyeon Lee <winkite1226@gmail.com>
2022-10-17 15:36:26 +0900

    complete user model

--- sparta/settings.py
+++ sparta/settings.py
@@ -48,7 +48,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'user',
     'post',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -139,3 +139,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.User'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Meta:
+        db_table = 'user'
+
+    user_id = models.CharField(max_length=30)
+    phone = models.IntegerField(default=0)
